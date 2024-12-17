29546b50544e2763e9fb3a0133f5ebcdea251425
lbabyuk <lenababyuk@gmail.com>
2024-12-18 00:05:57 +0200

    fix

--- js/countdown.js
+++ js/countdown.js
@@ -1,32 +1,38 @@
-const days = document.querySelector('#days');
-const hours = document.querySelector('#hours');
-const minutes = document.querySelector('#minutes');
-const seconds = document.querySelector('#seconds');
+const days = document.querySelector("#days");
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
 
-const newFinalTime = new Date("Jan 1, 2024 15:37:25").getTime();
-const finalTime = new Date("Jan 1, 2024 15:37:25");
-const expirationDay = document.querySelector('#expirationDay');
+const newFinalTime = new Date("March 17, 2025 15:37:25").getTime();
+const finalTime = new Date("March 17, 2025 15:37:25").toLocaleDateString(
+  "en-US",
+  {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+);
+const expirationDay = document.querySelector("#expirationDay");
 expirationDay.innerHTML = finalTime;
 
 const updateClockContainerTime = () => {
-    const currentTime = new Date().getTime();
-    const time = newFinalTime - currentTime;
+  const currentTime = new Date().getTime();
+  const time = newFinalTime - currentTime;
 
-    const day = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minute = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const second = Math.floor((time % (1000 * 60)) / 1000);
-    days.innerHTML = day;
-    hours.innerHTML = hour < 10 ? '0' + hour : hour;
-    minutes.innerHTML = minute < 10 ? '0' + minute : minute;
-    seconds.innerHTML = second < 10 ? '0' + second : second;
+  const day = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minute = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const second = Math.floor((time % (1000 * 60)) / 1000);
+  days.innerHTML = day;
+  hours.innerHTML = hour < 10 ? "0" + hour : hour;
+  minutes.innerHTML = minute < 10 ? "0" + minute : minute;
+  seconds.innerHTML = second < 10 ? "0" + second : second;
 
-    if(time < 0) {
-        const expirationDay = document.querySelector('#expirationDay');
-        clearInterval(updateClockContainerTime);
-        expirationDay.innerHTML = "Expired"
-    }
- }
+  if (time < 0) {
+    const expirationDay = document.querySelector("#expirationDay");
+    clearInterval(updateClockContainerTime);
+    expirationDay.innerHTML = "Expired";
+  }
+};
 
 setInterval(updateClockContainerTime, 1000);
-
